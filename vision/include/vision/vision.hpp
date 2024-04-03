f10b756e19547315ac570f62dac3472dc0a2fbf7
nilp amin <s4581164@student.uq.edu.au>
2024-04-03 10:49:12 +0000

    action_client version of vision.cpp

--- vision/include/vision/vision.hpp
+++ vision/include/vision/vision.hpp
@@ -2,7 +2,10 @@
 #define VISION_HPP
 
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
 #include <sensor_msgs/msg/image.hpp>
+
+#include "darknet_ros_msgs/action/check_for_objects.hpp"
 
 class Vision : public rclcpp::Node
 {
@@ -10,12 +13,16 @@ public:
     Vision();
 
 private:
-    void camera_callback(const sensor_msgs::msg::Image& msg);
+    void camera_callback(const sensor_msgs::msg::Image& msg);   //!< the camera feed callback
+
+    bool check_for_dog();    //!< used to trigger darknet action server
 
 private:
     sensor_msgs::msg::Image curr_image_;    //!< the current image from camera
 
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr camera_sub_;   //!< sub camera feed
+
+    rclcpp_action::Client<darknet_ros_msgs::action::CheckForObjects>::SharedPtr check_object_action_;     //!< action client to check for dog
 };
 
 
--- vision/src/vision.cpp
+++ vision/src/vision.cpp
@@ -1,13 +1,81 @@
 #include "vision/vision.hpp"
 
 Vision::Vision() : Node("vision_node") {
+    // create subscriber to camera feed
     camera_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
         "/image", 10, std::bind(&Vision::camera_callback, this, std::placeholders::_1)
     );
+
+    // create action client to check for dog in camera feed
+    check_object_action_ = rclcpp_action::create_client<darknet_ros_msgs::action::CheckForObjects>(
+        this, "CheckForObjects"
+    );
+
+    // wait for the action server to become available
+    if (!check_object_action_->wait_for_action_server(std::chrono::seconds(20)))
+    {
+        RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
+        // rclcpp::shutdown();
+    }
 }
 
 void Vision::camera_callback(const sensor_msgs::msg::Image& msg)
 {
     curr_image_ = msg;
-    RCLCPP_INFO(this->get_logger(), "I heard %i", msg.height);
+    RCLCPP_INFO(this->get_logger(), "dogs found %s", check_for_dog() ? "true" : "false");
+} 
+
+bool Vision::check_for_dog()
+{
+    auto goal = darknet_ros_msgs::action::CheckForObjects::Goal();
+    goal.id = 17; // the id of the dog in darknet
+    goal.image = curr_image_;
+    // create a promise to get the result of the action
+    auto goal_handle_future = check_object_action_->async_send_goal(goal);
+
+    // wait until it is accepted
+    if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), goal_handle_future) == 
+        rclcpp::FutureReturnCode::SUCCESS)
+    {
+        RCLCPP_ERROR(this->get_logger(), "send goal call failed.");
+        return false;
+    }
+
+    auto goal_handle = goal_handle_future.get();
+    if (!goal_handle)
+    {
+        RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server.");
+        return false;
+    }
+    
+    // wait for the server to be done with goal
+    auto result_future = check_object_action_->async_get_result(goal_handle);
+
+
+    RCLCPP_INFO(this->get_logger(), "Waiting for result");
+    if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result_future) !=
+        rclcpp::FutureReturnCode::SUCCESS)
+    {
+        RCLCPP_ERROR(this->get_logger(), "get result call failed.");
+        return false;
+    }
+
+    auto wrapped_result = result_future.get();
+
+    switch (wrapped_result.code) {
+        case rclcpp_action::ResultCode::SUCCEEDED:
+            break;
+        case rclcpp_action::ResultCode::ABORTED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+            return false;
+        case rclcpp_action::ResultCode::CANCELED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+            return false;
+        default:
+            RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+            return false;
+    }
+
+    RCLCPP_INFO(this->get_logger(), "result received");
+    return wrapped_result.result->bounding_boxes.bounding_boxes.size() > 0;
 } 
